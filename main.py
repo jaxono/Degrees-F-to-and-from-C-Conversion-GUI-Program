b1d845b772663c1dbe083d619385332e0484b3d0
jaxono <61395595+jaxono@users.noreply.github.com>
2021-03-08 14:46:09 +1300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 
 
+# A class that holds a window and some functions to manipulate it.
 class Window:
+	# A constructor for creating a window
 	def __init__(self, name):
 		self.border = tk.Tk()
 		self.border.title(name)
@@ -10,20 +12,32 @@ class Window:
 
 		self.elements = []
 
-	def pack_all(self):
-		for object in self.elements:
-			object.pack()
-
 
 def main():
-	main_window = Window("Hi")
+	# Convert °F to °C
+	def to_c():
+		num = float(text_box_val.get())
+		text_box_val.set((num - 30) / 2)
 
-	main_window.elements.append(tk.Label(main_window.frame, text="Label"))
+	# Convert °C to °F
+	def to_f():
+		num = float(text_box_val.get())
+		text_box_val.set(num * 2 + 30)
 
-	main_window.pack_all()
+	main_window = Window("Hi")  # Create Main Window
 
-	main_window.border.mainloop()
+	text_box_val = tk.StringVar()  # Holds text box value
+
+	text_box = tk.Entry(main_window.frame, width=50, textvariable=text_box_val)  # Text box
+	text_box.grid(row=0, column=0, columnspan=2)
+	to_c_button = tk.Button(main_window.frame, text="To °C", width=20, command=to_c)  # To °C button
+	to_c_button.grid(row=1, column=0)
+	to_f_button = tk.Button(main_window.frame, text="To °F", width=20, command=to_f)  # To °F button
+	to_f_button.grid(row=1, column=1)
+
+	main_window.border.mainloop()  # Enter a loop to keep the window open until the the X button is pressed.
 
 
+# Entrypoint
 if __name__ == "__main__":
 	main()
